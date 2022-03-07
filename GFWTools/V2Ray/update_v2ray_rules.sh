6e3308984b96fcaea032a63abaec3dc5404f4a1f
xiaokaixuan <xiaokaixuan@gmail.com>
2022-03-07 11:40:59 +0800

    Add files via upload

---
+++ GFWTools/V2Ray/update_v2ray_rules.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+let error_count=0
+while true; do
+	[ $error_count -lt 3 ] || exit 0
+	wget -qO geoip.new.dat https://proxy.go-back.cf/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+	[ "$?" = "0" ] && break
+	sleep 60
+	let error_count+=1
+done
+
+mv geoip.new.dat geoip.dat
+
+let error_count=0
+while true; do
+	[ $error_count -lt 3 ] || exit 0
+	wget -qO geosite.new.dat https://proxy.go-back.cf/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+	[ "$?" = "0" ] && break
+	sleep 60
+	let error_count+=1
+done
+
+mv geosite.new.dat geosite.dat
+
+systemctl restart v2ray.service v2ray-vless.service v2ray-vless-cf.service
+
