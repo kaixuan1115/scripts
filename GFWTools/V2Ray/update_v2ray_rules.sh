27b977eaa634a6fd436aee2ba1f3ca8317c41100
xiaokaixuan <xiaokaixuan@gmail.com>
2022-03-30 19:04:21 +0800

    优化 Router

--- GFWTools/V2Ray/update_v2ray_rules.sh
+++ GFWTools/V2Ray/update_v2ray_rules.sh
@@ -5,7 +5,7 @@ cd $(dirname "$0")
 let error_count=0
 while true; do
 	[ $error_count -lt 3 ] || exit 0
-	wget -qO geoip.new.dat https://proxy.go-back.cf/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+	wget -qO geoip.new.dat https://proxy.go-back.cf/https://github.com/xiaokaixuan/v2ray-rules-dat/releases/latest/download/geoip.dat
 	[ "$?" = "0" ] && break
 	sleep 60
 	let error_count+=1
@@ -16,7 +16,7 @@ mv geoip.new.dat geoip.dat
 let error_count=0
 while true; do
 	[ $error_count -lt 3 ] || exit 0
-	wget -qO geosite.new.dat https://proxy.go-back.cf/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+	wget -qO geosite.new.dat https://proxy.go-back.cf/https://github.com/xiaokaixuan/v2ray-rules-dat/releases/latest/download/geosite.dat
 	[ "$?" = "0" ] && break
 	sleep 60
 	let error_count+=1
