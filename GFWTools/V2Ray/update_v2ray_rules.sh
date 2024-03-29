894d74af9e82047d84d969bace1f146348591f3f
xiaokaixuan <xiaokaixuan@gmail.com>
2024-03-29 15:39:18 +0800

    Update update_v2ray_rules.sh

--- GFWTools/V2Ray/update_v2ray_rules.sh
+++ GFWTools/V2Ray/update_v2ray_rules.sh
@@ -4,25 +4,27 @@ cd $(dirname "$0")
 
 let error_count=0
 while true; do
-	[ $error_count -lt 3 ] || exit 0
-	wget -qO geoip.new.dat https://proxy.go-back.cf/https://github.com/xiaokaixuan/v2ray-rules-dat/releases/latest/download/geoip.dat
-	[ "$?" = "0" ] && break
+	[ $error_count -lt 3 ] || break
+	wget -qO geoip.new.dat https://proxy.111539.xyz/https://github.com/xiaokaixuan/v2ray-rules-dat/releases/latest/download/geoip.dat
+	[ "$?" == "0" ] && break
 	sleep 60
 	let error_count+=1
 done
 
-mv geoip.new.dat geoip.dat
+[ -s geoip.new.dat ] && cp -f geoip.new.dat geoip.dat
+rm -f geoip.new.dat
 
 let error_count=0
 while true; do
-	[ $error_count -lt 3 ] || exit 0
-	wget -qO geosite.new.dat https://proxy.go-back.cf/https://github.com/xiaokaixuan/v2ray-rules-dat/releases/latest/download/geosite.dat
-	[ "$?" = "0" ] && break
+	[ $error_count -lt 3 ] || break
+	wget -qO geosite.new.dat https://proxy.111539.xyz/https://github.com/xiaokaixuan/v2ray-rules-dat/releases/latest/download/geosite.dat
+	[ "$?" == "0" ] && break
 	sleep 60
 	let error_count+=1
 done
 
-mv geosite.new.dat geosite.dat
+[ -s geosite.new.dat ] && cp -f geosite.new.dat geosite.dat
+rm -f geosite.new.dat
 
 systemctl restart v2ray.service v2ray-vless.service v2ray-vless-cf.service
 
