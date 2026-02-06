f85533591fde213cd5ef87c3d9ab38c45c9e15d9
xiaokaixuan <xiaokaixuan@gmail.com>
2026-02-06 16:40:31 +0800

    Add commented command for SSL certificate generation
    Added a commented command for certificate generation.

--- GFWTools/SSL_certs/update_certs.sh
+++ GFWTools/SSL_certs/update_certs.sh
@@ -4,5 +4,6 @@ cd $(dirname "$0")
 
 exec 1> >(tee -a update_certs.log) 2>&1
 
+#./lego --email="kaixuan1115@126.com" --domains="111539.xyz" --http run
 ./lego --email="kaixuan1115@126.com" --domains="www.111111.xyz" --http renew --renew-hook="./myscript1.sh"
 
